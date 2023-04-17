f91d9d38537a3e4ec03a52d099cf5cde197494b2
over <80248568+over1234@users.noreply.github.com>
2023-04-17 10:11:10 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { conn } = require("./config/config");
 require("dotenv").config();
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
+const url = require('url');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
@@ -46,11 +47,7 @@ conn.connect((err) => {
     }
 })
 
-app.get('/', (req, res) => {
-    res.send('This Page is Index Page')
-})
-
-app.get('/oauth', async (req, res) => {
+app.get('/oauth', async(req, res) => {
     const authcode = req.query.code;
     if (authcode === undefined) {
         res.status(400).send('Authcode is required');
@@ -87,64 +84,72 @@ app.get('/oauth', async (req, res) => {
     }
     console.log(userInfo);
     conn.query(`select * from users where classNo = ? and studentNo = ?`, [userInfo.classNo, userInfo.studentNo],
-        (err, result) => {
-            if (err) {
-                console.log(err)
-                res.json({ message: err, success: false })
-            }
-            else if (result.length === 0) { // DB에 저장된 값이 없으면 => DB에 값 넣고 토큰 발급
-                const token = jwt.sign({ // 토큰 발급
-                    type: 'JWT',
-                    name: userInfo.name,
-                    grade: userInfo.grade,
-                    auth: true
-                },
-                    "secretkey",
-                    {
-                        subject: 'certification',
-                        issuer: 'over'
+    (err, result) => {
+        if (err) {
+            console.log(err)
+            res.json({ message: err, success: false })
+        }
+        else if (result.length === 0) { // DB에 저장된 값이 없으면 => DB에 값 넣고 토큰 발급
+            const token = jwt.sign({ // 토큰 발급
+                type: 'JWT',
+                name: userInfo.name,
+                grade: userInfo.grade,
+                auth : true
+            }, 
+            "secretkey",
+            {
+                subject: 'certification',
+                issuer: 'over'
+            });
+            conn.query(`insert into users(token, grade, classNo, studentNo, name) values(?,?,?,?,?)`, 
+            [token, userInfo.grade, userInfo.classNo, userInfo.studentNo, userInfo.name], 
+            (err, result) => { // insert
+                if (err) {
+                    console.log(err)
+                    res.json({ message: err, success: false })
+                }
+                else if (result) {
+                    const query = querystring.stringify({
+                        "user_token": token,
+                        "success": true
                     });
-                conn.query(`insert into users(token, grade, classNo, studentNo, name) values(?,?,?,?,?)`,
-                    [token, userInfo.grade, userInfo.classNo, userInfo.studentNo, userInfo.name],
-                    (err, result) => { // insert
-                        if (err) {
-                            console.log(err)
-                            res.json({ message: err, success: false })
-                        }
-                        else if (result) {
-                            return res.json({
-                                message: 'login successful',
-                                user_token: token,
-                                success: true
-                            })
-                        }
-                    })
-            }
-            else { // 이미 DB에 저장된 유저 => DB에서 찾아서 리턴
-                conn.query(`select * from users 
-            where classNo = ? and studentNo = ?`, [userInfo.classNo, userInfo.studentNo],
-                    (err, result) => {
-                        if (err) {
-                            res.json({ 'massage': err, success: false });
-                        } else {
-                            console.log('이미 DB에 저장된 유저임.')
-                            return res.json({
-                                message: 'login successful',
-                                user_token: token,
-                                success: true
-                            })
-                        }
+                    return res.redirect('/login_success?' + query)
+                }
+            })
+        }
+        else { // 이미 DB에 저장된 유저 => DB에서 찾아서 리턴
+            conn.query(`select * from users 
+            where classNo = ? and studentNo = ?`, [userInfo.classNo, userInfo.studentNo], 
+            (err, result) => {
+                if (err) {
+                    res.json({ 'massage': err, success: false });
+                } else {
+                    console.log('이미 DB에 저장된 유저임.')
+                    const query = querystring.stringify({
+                        "user_token": result[0].token,
+                        "success": true
                     });
-            }
-        })
+                    return res.redirect('/login_success?' + query)
+                }
+            });
+        }
+    })
 });
+
+app.get('/login_success', (req, res) => {
+    res.json({
+        message: 'login success!',
+        user_token : req.query.token,
+        success : req.query.success
+    })
+})
 
 app.post('/user_info', (req, res) => { // 사용자 정보 리턴 해주는 API
     const token = req.body.token;
     try {
         var check = jwt.verify(token, "secretkey",);
         if (check) { // 토큰 값이 유효한지 체크
-            console.log(check.auth);
+            console.log(check.auth); 
             if (check.auth) { // auth가 true인지 체크 -> 이상한 토큰 값을 거름
                 conn.query(`select * from users where token = ?`, [token], (err, result) => { // 안전한 토큰 값을 확인한 후에 쿼리
                     if (err) { // 단순 error 처리문
@@ -185,7 +190,7 @@ app.post('/login_check', (req, res) => { // 로그인 무결성 검사 API
                 res.json({ message: '토큰이 유호하지 못하거나 사용자의 토큰이 아님.', success: false })
             }
             else { // 오 너 로그인 했구나!?
-                res.json({ message: '토큰이 유호함. 로그인한 유저임.', success: true })
+                res.json({message: '토큰이 유호함. 로그인한 유저임.', success: true})
             }
         })
 })
@@ -223,61 +228,61 @@ app.post('/reservation', (req, res) => { // 예약 처리 API(C)
     const token = req.body.token
 
     conn.query(`select id from users where token = ?`, [token],
-        (err, result) => {
-            const userId = result[0].id
-            if (err) { // 단순 error 처리문
-                console.log(err)
-                res.json({ message: err, success: false })
-            }
-            else {
-                conn.query(`insert into reservation(userId, seat, reservationDay, expirationDay) values(?,?,?,?)`,
-                    [userId, seatNo, reservationDay, expirationDay],
-                    (err, result) => {
-                        if (err) { // 단순 error 처리문
-                            console.log(err)
-                            res.json({ message: err, success: false })
-                        } else {
-                            res.json({ message: '좌석 예약 완료', success: true })
-                        }
-                    })
-            }
-        })
+    (err, result) => {
+        const userId = result[0].id
+        if (err) { // 단순 error 처리문
+            console.log(err)
+            res.json({ message: err, success: false })
+        }
+        else {
+            conn.query(`insert into reservation(userId, seat, reservationDay, expirationDay) values(?,?,?,?)`,
+            [userId, seatNo, reservationDay, expirationDay],
+            (err, result) => {
+                if (err) { // 단순 error 처리문
+                    console.log(err)
+                    res.json({ message: err, success: false })
+                } else {
+                    res.json({ message: '좌석 예약 완료', success: true })
+                }
+            })
+        }
+    })
 })
 
 app.post('/ticket', (req, res) => { // 티켓 발급 API(R)
     const token = req.body.token
-
+    
     conn.query(`select * from users where token = ?`, [token], // 토큰으로 유저 데이터 들고오기
-        (err, result) => {
-            const grade = result[0].grade;
-            const classNo = result[0].classNo;
-            const studentNo = result[0].studentNo;
-            const name = result[0].name;
-            if (err) { // 단순 error 처리문
-                console.log(err)
-                res.json({ message: err, success: false })
-            } else {
-                conn.query(`select * from reservation where userId = ?`, [result[0].id], // 외래키 이용 유저가 예약한 정보 들고오기
-                    (err, result) => {
-                        if (err) { // 단순 error 처리문
-                            console.log(err)
-                            res.json({ message: err, success: false })
-                        } else {
-                            return res.json({ // 티켓 발급에 필요한 데이터 리턴
-                                grade: grade,
-                                classNo: classNo,
-                                studentNo: studentNo,
-                                name: name,
-                                userId: result[0].userId,
-                                seatNo: result[0].seat,
-                                reservationDay: result[0].reservationDay,
-                                expirationDay: result[0].expirationDay,
-                                success: true
-                            })
-                        }
+    (err, result) => {
+        const grade = result[0].grade;
+        const classNo = result[0].classNo;
+        const studentNo = result[0].studentNo;
+        const name = result[0].name;
+        if (err) { // 단순 error 처리문
+            console.log(err)
+            res.json({ message: err, success: false })
+        } else {
+            conn.query(`select * from reservation where userId = ?`, [result[0].id], // 외래키 이용 유저가 예약한 정보 들고오기
+            (err, result) => {
+                if (err) { // 단순 error 처리문
+                    console.log(err)
+                    res.json({ message: err, success: false })
+                } else {
+                    return res.json({ // 티켓 발급에 필요한 데이터 리턴
+                        grade: grade,
+                        classNo: classNo,
+                        studentNo: studentNo,
+                        name: name,
+                        userId: result[0].userId,
+                        seatNo: result[0].seat,
+                        reservationDay: result[0].reservationDay,
+                        expirationDay: result[0].expirationDay,
+                        success: true
                     })
-            }
-        })
+                }
+            })
+        }
+    })
 })
 
 app.patch('/reservation_update', (req, res) => { // 예약 업데이트(U)
@@ -288,29 +293,29 @@ app.patch('/reservation_update', (req, res) => { // 예약 업데이트(U)
     conn.query(`UPDATE reservation
                 SET seat = ?, reservationDay = ?
                 WHERE userId = ?`,
-        [updateSeatNo, updateDay, userId],
-        (err, result) => {
-            if (err) { // 단순 error 처리문
-                console.log(err)
-                res.json({ message: err, success: false })
-            } else {
-                console.log(result)
-                res.json({ message: `${updateSeatNo}로 좌석이 업데이트 되었음.`, success: true })
-            }
-        })
+                [updateSeatNo, updateDay, userId],
+                (err, result) => {
+                    if (err) { // 단순 error 처리문
+                        console.log(err)
+                        res.json({ message: err, success: false })
+                    } else {
+                        console.log(result)
+                        res.json({message: `${updateSeatNo}로 좌석이 업데이트 되었음.`, success: true})
+                    }
+            })
 })
 
 app.delete('/reservation_cancel', (req, res) => { // 예약 취소(D)
     const seatNo = req.query.seatNo;
-    conn.query(`DELETE FROM reservation where seat = ?`, [seatNo],
-        (err, result) => {
-            if (err) { // 단순 error 처리문
-                console.log(err)
-                res.json({ message: err, success: false })
-            } else {
-                res.json({ message: `좌석이 예약 취소 되었음.`, success: true })
-            }
-        })
+    conn.query(`DELETE FROM reservation where seat = ?`, [seatNo], 
+    (err, result) => {
+        if (err) { // 단순 error 처리문
+            console.log(err)
+            res.json({ message: err, success: false })
+        } else {
+            res.json({ message: `좌석이 예약 취소 되었음.`, success: true })
+        }
+    })
 })
 
 app.listen(80);
